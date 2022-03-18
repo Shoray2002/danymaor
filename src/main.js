11eabbf7c66375a2bd224af6abdfcc6fa860ee32
Shoray Singhal <lci2020037@iiitl.ac.in>
2022-03-18 16:01:53 +0530

    done

--- src/main.js
+++ src/main.js
@@ -2,28 +2,49 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 
-let camera, scene, renderer, controls;
+// importing the threejs library from the node_modules folder
+// importing the OrbitControls that allows the camera to move around the scene using the mouse
+// importing the FBXLoader which is used to load the FBX model file
+
+let camera, scene, renderer, controls; //basic variables
+// variables to keep track of the movement of the camera
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
+
+// function calls
 init();
 animate();
+
+// function to initialize the scene
 function init() {
+  // creating a new div in the html file to display the scene
   const container = document.createElement("div");
   document.body.appendChild(container);
+
+  // initializing the camera
+  // https://threejs.org/docs/?q=camera#api/en/cameras/PerspectiveCamera
   camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     1,
     1000
   );
-  camera.position.set(0, 50, 100);
+  camera.position.set(-80, 100, 200);
+
+  // initializing the scene
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x6d9ec8);
+
+  // setting the lights
+
+  // https://threejs.org/docs/?q=hemis#api/en/lights/HemisphereLight
   const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
   hemiLight.position.set(0, 200, 0);
   scene.add(hemiLight);
+
+  // https://threejs.org/docs/?q=dire#api/en/lights/DirectionalLight
   const dirLight = new THREE.DirectionalLight(0xffffff);
   dirLight.position.set(0, 200, 100);
   dirLight.castShadow = true;
@@ -33,38 +54,40 @@ function init() {
   dirLight.shadow.camera.right = 120;
   scene.add(dirLight);
 
-  // ground
+  // initializing the ground
   const mesh = new THREE.Mesh(
     new THREE.PlaneGeometry(2000, 2000),
-    new THREE.MeshPhongMaterial({ color: 0x6d9ec8, depthWrite: false })
+    new THREE.MeshPhongMaterial({ color: 0x6d9ec8, side: THREE.DoubleSide })
   );
+
   mesh.rotation.x = -Math.PI / 2;
   mesh.receiveShadow = true;
   scene.add(mesh);
-  // grid
-  const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-  grid.material.opacity = 0.2;
-  grid.material.transparent = true;
-  scene.add(grid);
+
+  // loading the FBX model and texture
   const textureLoader = new THREE.TextureLoader();
   const texture = textureLoader.load("../assets/texture.png");
-  const loader = new FBXLoader();
-  loader.load("../assets/apartment.fbx", function (object) {
+
+  const model = new FBXLoader();
+  model.load("../assets/apartment.fbx", function (object) {
     console.log(object);
+    // modifying the FBX model
     object.traverse(function (child) {
       if (child.isMesh) {
-        child.castShadow = true;
-        child.receiveShadow = true;
-        child.material.map = texture;
-        child.material.needsUpdate = true;
-        child.material.side = THREE.DoubleSide;
+        child.castShadow = true; // cast shadow
+        child.receiveShadow = true; // receive shadow
+        child.material.map = texture; // setting the texture onto the model
+        child.material.needsUpdate = true; // updating the material
+        child.material.side = THREE.DoubleSide; // making the texture visible from both sides of the model surface
       }
     });
-    object.scale.set(10, 10, 10);
+    // increasing the size of the model
+    object.scale.set(20, 20, 20);
     object.position.set(0, 0, 0);
     scene.add(object);
   });
 
+  // initializing the renderer
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -72,14 +95,19 @@ function init() {
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   container.appendChild(renderer.domElement);
 
+  // initializing the controls
   controls = new OrbitControls(camera, renderer.domElement);
   controls.target.set(0, 0, 0);
+  controls.enableZoom = false; // disabling the zoom controls
   controls.update();
+
+  // adding the event listeners
   window.addEventListener("resize", onWindowResize, false);
   window.addEventListener("keydown", onKeyDown, false);
   window.addEventListener("keyup", onKeyUp, false);
 }
 
+// function to resize the scene
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -87,6 +115,7 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+// function when a key is pressed
 function onKeyDown(event) {
   switch (event.keyCode) {
     case 38: // up
@@ -108,6 +137,7 @@ function onKeyDown(event) {
   }
 }
 
+// function when a key is released
 function onKeyUp(event) {
   switch (event.keyCode) {
     case 38: // up
@@ -129,7 +159,9 @@ function onKeyUp(event) {
   }
 }
 
+// function to animate the scene
 function animate() {
+  // move the camera if any of the movement keys are pressed
   if (moveForward) {
     camera.translateZ(-1);
   }
